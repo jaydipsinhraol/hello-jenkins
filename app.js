b1becc017d2ab9b3344947617d533f189341ab90
jaydipsinh <jaydipsinh.raol@rapidops.com>
2016-11-18 15:01:56 +0530

    changes done

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('hello world');
 });
- 
+app.get('/hello',function(req, res) {
+    res.send("hey guys");
+});
+
 app.listen(process.env.PORT || 5000);
  
 module.exports = app;
